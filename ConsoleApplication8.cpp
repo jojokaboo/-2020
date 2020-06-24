12421850a17de93a4afeda3dd9cfee24951f3183
jojokaboo <67367598+jojokaboo@users.noreply.github.com>
2020-06-24 12:42:24 +0300

    Add files via upload
    Добавление файлов на сайт.

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,25 @@
+﻿/**
+*@file  ConsoleApplication8.cpp
+*@author Селіванова М.О., гр. 515і
+*@date 19 июня 2020
+*@brief Практическая работа
+*
+*/
+#include "Header.h"
+
+int main() {
+	int i;
+	int h;
+	int number;
+	int j[RAND_MAX];
+	printf("1.Manual input\n"); //выбор ввод пользователем или рандомный
+	printf("2.Random numbers\n");
+	scanf_s("%d", &h);
+	switch (h)	//В зависимости от выбора вызывается функция, обе записаны в inputs.cpp
+	{
+	case 1: user(j); break;
+	case 2: random(j); break;
+	}
+	find_zeros(j);	//Вызов функции из findzeros.cpp
+	return 0;
+}
---
+++ inputs.cpp
@@ -0,0 +1,24 @@
+/**
+*@file  inputs.cpp
+*@author Селіванова М.О., гр. 515і
+*@date 19 июня 2020
+*@brief Практическая работа
+*
+*/
+#include "Header.h"
+
+void random(int j[]) {
+	srand(time(0));
+	int i;
+	for (i = 0; i < RAND_MAX; i++) {	//Генерируется рандомный список чисел
+		j[i] = rand() % 21 - 10;		//Промежуток можно задавать просчитав
+		printf("%d ", j[i]);
+	}
+}
+
+void user(int j[]) {
+	printf("Input %i numbers: ", RAND_MAX);	//Пользователь вводит числа в массив 
+	for (int i = 0; i < RAND_MAX; i++) {
+		scanf_s("%d", &j[i]);				//Запись чисел в массив
+	}
+}
